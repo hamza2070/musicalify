61b9389b29adc6fb22e05d8ce8c1ea27335bc282
Hamza Shah <hamza.207029@gmail.com>
2025-01-29 17:31:12 +0500

    Initial Commit

---
+++ src/misc/color.js
@@ -0,0 +1,14 @@
+export default {
+  APP_BG: '#121212',        // Dark background
+  FONT: '#E0E0E0',         // Light font for contrast
+  FONT_MEDIUM: '#B3B3B3',  // Medium-light font
+  FONT_LIGHT: '#808080',   // Dimmed font for less emphasis
+  MODAL_BG: 'rgba(255, 255, 255, 0.1)',  // Light overlay for modals
+  ACTIVE_BG: '#1DB954',    // Spotify-like green for active state
+  ACTIVE_FONT: '#FFFFFF',  // White font for active state
+  TAB_BAR_BG: '#181818',   // Dark tab bar background
+  TAB_BAR_ACTIVE: '#1DB954', // Active tab color
+  TAB_BAR_INACTIVE: '#B3B3B3', // Inactive tab color
+  HEADER_BG: '#1E1E1E',    // Dark header background
+  HEADER_FONT: '#FFFFFF',  // Header text color
+};
